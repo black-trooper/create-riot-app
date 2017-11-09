0e5a0e164db3f8dba571532b21e3804817f8aec1
k_kuwahara <k_kuwahara@yumemi.co.jp>
2017-11-09 12:37:20 +0900

    add issue template

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,8 @@
+# Describe your issue
+
+# Which version of Riot does it affect?
+
+# Please teach me about your environment
+
+- `node -v`: 
+- `npm -v`:
--- commands/version.js
+++
@@ -1,5 +0,0 @@
-const package_json = require('./package.json')
-version = () => {
-  process.stdout.write(package_json.version + '\n')
-}
-export.version = version
